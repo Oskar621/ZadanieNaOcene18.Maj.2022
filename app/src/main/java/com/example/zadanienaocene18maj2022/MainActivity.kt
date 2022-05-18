f5d94fe9ee138851e1731e64f3e55aed895570fa
sztoslol <kubuszeski05@wp.pl>
2022-05-18 07:49:24 +0200

    Dodano: kod do 2 buttonow nastepny i poprzedni, ktory zmienia wyswietlany obrazek.

--- app/src/main/java/com/example/zadanienaocene18maj2022/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene18maj2022/MainActivity.kt
@@ -2,10 +2,92 @@ package com.example.zadanienaocene18maj2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nrObrazka = findViewById<TextView>(R.id.numer)
+        val poprzedniButton = findViewById<Button>(R.id.poprzedni)
+        val nastButton = findViewById<Button>(R.id.nastepny)
+        val obrazekid = findViewById<ImageView>(R.id.Obrazki)
+        var obraz = 1
+        poprzedniButton.setOnClickListener {
+            obraz -= 1
+            when(obraz)
+            {
+                0 ->
+                    {
+                        obraz = 5
+                        obrazekid.setImageResource(R.drawable.pies5)
+                        nrObrazka.text = "5";
+                    }
+                1 -> {
+                    obrazekid.setImageResource(R.drawable.pies1)
+                    nrObrazka.text = "1";
+                    }
+                2 -> {
+                    obrazekid.setImageResource(R.drawable.pies2)
+                    nrObrazka.text = "2";
+                }
+                3 -> {
+                    obrazekid.setImageResource(R.drawable.pies3)
+                    nrObrazka.text = "3";
+                }
+                4 -> {
+                    obrazekid.setImageResource(R.drawable.pies4)
+                    nrObrazka.text = "4";
+                }
+                5 -> {
+                    obrazekid.setImageResource(R.drawable.pies5)
+                    nrObrazka.text = "5";
+                }
+                6 -> {
+                    obraz = 1
+                    obrazekid.setImageResource(R.drawable.pies1)
+                    nrObrazka.text = "1";
+                }
+            }
+        }
+        nastButton.setOnClickListener {
+            obraz += 1
+            when(obraz)
+            {
+                0 -> {
+                    obraz = 5
+                    obrazekid.setImageResource(R.drawable.pies5)
+                    nrObrazka.text = "5";
+                }
+
+                1 -> {
+                    obrazekid.setImageResource(R.drawable.pies1)
+                    nrObrazka.text = "1";
+                }
+                2 -> {
+                    obrazekid.setImageResource(R.drawable.pies2)
+                    nrObrazka.text = "2";
+                }
+                3 -> {
+                    obrazekid.setImageResource(R.drawable.pies3)
+                    nrObrazka.text = "3";
+                }
+                4 -> {
+                    obrazekid.setImageResource(R.drawable.pies4)
+                    nrObrazka.text = "4";
+                }
+                5 -> {
+                    obrazekid.setImageResource(R.drawable.pies5)
+                    nrObrazka.text = "5";
+                }
+                6 -> {
+                    obraz = 1
+                    obrazekid.setImageResource(R.drawable.pies1)
+                    nrObrazka.text = "1";
+                }
+            }
+        }
     }
 }
